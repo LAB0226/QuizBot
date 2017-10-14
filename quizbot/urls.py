d816bd9e5fc37d9325104408d66aace47be50393
Shirokuma <maroron4513@gmail.com>
2017-10-14 05:03:30 +0000

    nagao 設定調整

--- quizbot/urls.py
+++ quizbot/urls.py
@@ -20,5 +20,6 @@ from chat_app import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^callback', views.callback)
+    url(r'^index/', views.index),
+    url(r'^callback/', views.callback)
 ]
